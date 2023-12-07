23768ffa7d8c2226505ef0cc33fcc8707e62178b
JaguarDrinks <facundorossetto@gmail.com>
2023-12-07 10:46:57 +0000

    JS & more styles Starting to instantiate HTML elements on Javascript document, creaaating variables & adding more styles on CSS document to section 2.

---
+++ main.js
@@ -0,0 +1,17 @@
+/* Variables: */
+
+let without = document.querySelector(".without")
+let addDiv = document.getElementById("add-div")
+let btnIcon = document.getElementById("btn-icon")
+let task = document.getElementById("task")
+let addBtn = document.getElementById("add-btn")
+let days = document.getElementById("days")
+let iconDiv = document.getElementById("icon-div")
+let iconRow = document.querySelectorAll(".icon-row")
+const iconImages = document.querySelectorAll(".icon")
+let myTasks = document.getElementById("my-tasks")
+let container = document.querySelector(".container")
+let plusButton = document.getElementById("plus-button")
+
+
+
